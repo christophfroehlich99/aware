409f927709c02909454b517229e6dff90f9b9693
Christoph Fröhlich <christophfroehlich99@web.de>
2022-07-11 12:19:07 +0200

    basic register error handling

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+    default: "lastName",
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    default: "unauthorized",
+  },
+});
+
+export default mongoose.model("User", UserSchema);
